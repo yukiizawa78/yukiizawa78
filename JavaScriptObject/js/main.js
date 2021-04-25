0867d9f627d3df7191792c7a00a8e81af4d48c54
yukiizawa78 <artdirector1980@gmail.com>
2021-04-25 15:17:42 +0900

    use delete point.y; point.z = 90;

--- JavaScriptObject/js/main.js
+++ JavaScriptObject/js/main.js
@@ -1,9 +1,17 @@
 'use strict';
 
 {
-  const mousepoint = {
+  const point = {
     x: 100,
     y: 180,
   };
-  console.log(mousepoint);
+
+point.x = 120;
+
+  // console.log(point.x);
+  // console.log(point['y']);
+
+  point.z = 90;
+  delete point.y;
+  console.log(point);
 }
