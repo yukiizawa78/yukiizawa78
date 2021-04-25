72e52a1d4ab36011610a061d0f5e89055760b6aa
yukiizawa78 <artdirector1980@gmail.com>
2021-04-25 10:43:19 +0900

    use boolean

--- js/main.js
+++ js/main.js
@@ -1,9 +1,16 @@
 'use strict';
 
-// console.log('5' * 3);
-// console.log('5' - '3');
+const price = 1200;
 
-// console.log('5' + 3);
-// console.log(parseInt('5', 10) + 3);
+// console.log(price > 1000); // true
+// console.log(price < 1000); // false
+// console.log(price >= 1000); // true
+// console.log(price <= 1000); // false
+// console.log(price === 1000); // false
+// console.log(price !== 1000); // true
 
-console.log(parseInt('hello', 10));
+// false <- 0, null, undefined, '', false
+// true <- それ以外
+
+console.log(Boolean(0));
+console.log(Boolean('hello'));
