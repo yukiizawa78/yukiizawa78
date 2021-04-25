b7c09c45d26d110f946973412b6773046bfa9a31
yukiizawa78 <artdirector1980@gmail.com>
2021-04-25 10:40:15 +0900

    use typeof

--- js/main.js
+++ js/main.js
@@ -1,20 +1,7 @@
 'use strict';
 
-// 変数 let
-
-let price = 500;
-
-// price = price + 100;
-price += 100; // 600
-
-// price = price * 2;
-price *= 2; // 1200
-
-// price = price + 1;
-// price += 1;
-price++; // 1201
-
-// price -= 1;
-price--; // 1200
-
-console.log(price);
+console.log(typeof 'hello');
+console.log(typeof 5);
+console.log(typeof true);
+console.log(typeof undefined);
+console.log(typeof null);
