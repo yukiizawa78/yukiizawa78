0c9880e86e47282b5b0fdb66e3496bb8e20d178f
yukiizawa78 <artdirector1980@gmail.com>
2021-04-25 10:52:27 +0900

    use continue, break

--- js/main.js
+++ js/main.js
@@ -1,15 +1,12 @@
 'use strict';
 
-// let hp = 100;
-
-// while (hp > 0) {
-//   console.log(`${hp} HP left!`);
-//   hp -= 15;
-// }
-
-let hp = -50;
-
-do {
-  console.log(`${hp} HP left!`);
-  hp -= 15;
-} while (hp > 0);
+for (let i = 1; i <= 10; i++) {
+  // if (i === 4) {
+  // if (i % 3 === 0) {
+  //   continue;
+  // }
+  if (i === 4) {
+    break;
+  }
+  console.log(i);
+}
