e866ff459bb6b42cae15f502f8b8909f93f83a81
yukiizawa78 <artdirector1980@gmail.com>
2021-04-25 11:15:48 +0900

    use array and length

--- JavaScriptObject/js/main.js
+++ JavaScriptObject/js/main.js
@@ -1,10 +1,14 @@
 'use strict';
 
 {
-  // const score1 = 80;
-  // const score2 = 90;
-  // const score3 = 40;
-
   const scores = [80, 90, 40];
+  // console.log(scores[1]);
+ 
+  scores[2] = 44;
   console.log(scores);
+
+  // scores = 10; 直接代入はできない
+
+  console.log(scores.length)
+
 }
