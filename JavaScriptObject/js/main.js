a2200b9d6d123a100d066086632ef13247adf7ed
yukiizawa78 <artdirector1980@gmail.com>
2021-04-25 18:24:45 +0900

    use  let y = [...x];

--- JavaScriptObject/js/main.js
+++ JavaScriptObject/js/main.js
@@ -7,9 +7,15 @@
   // console.log(x); //5
   // console.log(y); //1
  
+  // let x = [1, 2];
+  // let y = x;
+  // x[0] = 5;
+  // console.log(x); //[5, 2]
+  // console.log(y); //[5, 2]
+
   let x = [1, 2];
-  let y = x;
+  let y = [...x];
   x[0] = 5;
   console.log(x); //[5, 2]
-  console.log(y); //[5, 2]
+  console.log(y); //[1, 2]
 }
