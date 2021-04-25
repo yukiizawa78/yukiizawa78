681ea30a3ac1fe44f063588332ae1f6cc9de66bc
yukiizawa78 <artdirector1980@gmail.com>
2021-04-25 10:56:38 +0900

    use const sum = function(a, b, c)

--- js/main.js
+++ js/main.js
@@ -1,13 +1,12 @@
 'use strict';
 
-function sum(a, b, c) {
-  // console.log(a + b + c);
+// function sum(a, b, c) {
+//   return a + b + c;
+// }
+
+const sum = function(a, b, c) {
   return a + b + c;
-  console.log(a + b + c);
-}
+};
 
-// sum(1, 2, 3);
-// sum(3, 4, 5);
-
-const total = sum(1, 2, 3) + sum(3, 4, 5); // 18
-// console.log(total);
+const total = sum(1, 2, 3) + sum(3, 4, 5);
+console.log(total);
