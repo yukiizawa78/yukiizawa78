2275fd2809f03d13539484f92a6c5a4c5ca24f63
yukiizawa78 <artdirector1980@gmail.com>
2021-04-25 10:39:39 +0900

    use ++ --

--- js/main.js
+++ js/main.js
@@ -1,12 +1,20 @@
 'use strict';
 
-// 定数 const
-// 変数 let var
+// 変数 let
 
-let price = 150;
-console.log(price * 140);
-console.log(price * 160);
+let price = 500;
 
-price = 170;
-console.log(price * 140);
-console.log(price * 160);
+// price = price + 100;
+price += 100; // 600
+
+// price = price * 2;
+price *= 2; // 1200
+
+// price = price + 1;
+// price += 1;
+price++; // 1201
+
+// price -= 1;
+price--; // 1200
+
+console.log(price);
