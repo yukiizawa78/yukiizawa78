2c3445c5eef7373e48f09ca9e45d03586bba2701
yukiizawa78 <artdirector1980@gmail.com>
2021-04-25 10:41:07 +0900

    use parseInt

--- js/main.js
+++ js/main.js
@@ -1,7 +1,9 @@
 'use strict';
 
-console.log(typeof 'hello');
-console.log(typeof 5);
-console.log(typeof true);
-console.log(typeof undefined);
-console.log(typeof null);
+// console.log('5' * 3);
+// console.log('5' - '3');
+
+// console.log('5' + 3);
+// console.log(parseInt('5', 10) + 3);
+
+console.log(parseInt('hello', 10));
