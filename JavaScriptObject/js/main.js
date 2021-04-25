81b138e21f2d3979e5fa626fc95cb15c1fe1bbd5
yukiizawa78 <artdirector1980@gmail.com>
2021-04-25 15:10:33 +0900

    use const point = {x: 100, y: 180};

--- JavaScriptObject/js/main.js
+++ JavaScriptObject/js/main.js
@@ -1,15 +1,9 @@
 'use strict';
 
 {
-  const numbers = [1, 4, 7, 8, 10];
-//   const evenNumbers = numbers.filter(number => {
-// if (number % 2 === 0) {
-//   return true;
-// } else {
-//   return false;
-// }
-//   });
-
-  const evenNumbers = numbers.filter(number => number % 2 === 0);
-  console.log(evenNumbers);
+  const mousepoint = {
+    x: 100,
+    y: 180,
+  };
+  console.log(mousepoint);
 }
