7820b0314ebd4d3d4c04b92cbce96ac2996f99b8
yukiizawa78 <artdirector1980@gmail.com>
2021-04-25 22:29:45 +0900

    use this.todos.splice(index, 1);

--- MyVueApp/js/main.js
+++ MyVueApp/js/main.js
@@ -12,13 +12,14 @@
       ]
     },
     methods: {
-      // addItem: function(e) {
-      //   e.preventDefault();
-      //   this.todos.push(this.newItem);
-      // }
       addItem: function() {
         this.todos.push(this.newItem);
         this.newItem = '';
+      },
+      deleteItem: function(index) {
+        if (confirm('are you sure?')) {
+          this.todos.splice(index, 1);
+        }
       }
     }
   });
