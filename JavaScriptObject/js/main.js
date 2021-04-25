2783fdbdaa1bdea6a820e6afa37a742160e22305
yukiizawa78 <artdirector1980@gmail.com>
2021-04-25 19:39:33 +0900

    use d.setDate(d.getDate() + 3);

--- JavaScriptObject/js/main.js
+++ JavaScriptObject/js/main.js
@@ -2,8 +2,9 @@
 
 {
 
-  const d = new Date();
-  // console.log(d);
-
-  console.log(`${d.getMonth() + 1} 月 ${d.getDate()} 日`)
+  const d = new Date(2019, 10);
+  d.setHours(10, 20, 30);
+  d.setDate(31);
+  d.setDate(d.getDate() + 3);
+  console.log(d);
 }
