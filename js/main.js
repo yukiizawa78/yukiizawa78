d71bec2faddda211e38aac35fd3096534692e121
yukiizawa78 <artdirector1980@gmail.com>
2021-04-25 10:49:01 +0900

    use for

--- js/main.js
+++ js/main.js
@@ -1,27 +1,7 @@
 'use strict';
 
-const signal = 'pink';
-
-// if (signal === 'red') {
-//   console.log('Stop!');
-// } else if (signal === 'yellow') {
-//   console.log('Caution!');
-// } else if (signal === 'blue'){
-//   console.log('Go!');
-// }
-
-switch (signal) {
-  case 'red':
-    console.log('Stop!');
-    break;
-  case 'yellow':
-    console.log('Caution!');
-    break;
-  case 'blue':
-  case 'green':
-    console.log('Go!');
-    break;
-  default:
-    console.log('Wrong signal!');
-    break;
+for (let i = 1; i <= 10; i++) {
+  // console.log('hello');
+  // console.log('hello' + i);
+  console.log(`hello ${i}`);
 }
