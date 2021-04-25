1316ce49f353c6adb3b70adf161e32c837c4acf0
yukiizawa78 <artdirector1980@gmail.com>
2021-04-25 10:59:24 +0900

    understand scope

--- js/main.js
+++ js/main.js
@@ -1,15 +1,11 @@
 'use strict';
 
-// const sum = function(a, b, c) {
-//   return a + b + c;
-// };
-// const sum = (a, b, c) => a + b + c;
+const x = 2;
 
-// const total = sum(1, 2, 3) + sum(3, 4, 5);
-// console.log(total);
+function f() {
+  // const x = 1;
+  console.log(x);
+}
 
-// const double = function(a) {
-//   return a * 2;
-// };
-const double = a => a * 2;
-console.log(double(12));
+f();
+console.log(x);
