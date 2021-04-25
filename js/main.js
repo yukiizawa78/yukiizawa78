5b14d40a316d0c1dde486eb59f6ccb2edd250981
yukiizawa78 <artdirector1980@gmail.com>
2021-04-25 10:38:28 +0900

    use const

--- js/main.js
+++ js/main.js
@@ -1,11 +1,11 @@
 'use strict';
 
-// console.log(10 + 3); // 13
-// console.log(10 - 3); // 7
-// console.log(10 * 3); // 30
-// console.log(10 / 3); // 3.333...
-// console.log(10 % 3); // 1
-// console.log(10 ** 3); // 1000
+// 定数
 
-console.log(2 + 10 * 3); // 32
-console.log((2 + 10) * 3); // 36
+// console.log(150 * 140);
+// console.log(150 * 160);
+
+const price = 150;
+
+console.log(price * 140);
+console.log(price * 160);
