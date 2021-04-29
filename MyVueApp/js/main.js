5baec581e2855a5ec20233821a4731734aef0952
yukiizawa78 <artdirector1980@gmail.com>
2021-04-29 18:21:24 +0900

    add purge

--- MyVueApp/js/main.js
+++ MyVueApp/js/main.js
@@ -29,14 +29,26 @@
         if (confirm('are you sure?')) {
           this.todos.splice(index, 1);
         }
+      },
+      purge: function() {
+        if (!confirm('delete finished?')) {
+          return;
+        }
+        // this.todos = this.todos.filter(function(todo) {
+        //   return !todo.isDone;
+        // }); 
+        this.todos = this.remaining;
       }
     },
     computed: {
       remaining: function() {
-        var items = this.todos.filter(function(todo) {
+        // var items = this.todos.filter(function(todo) {
+        //   return !todo.isDone;
+        // });
+        // return items.length;
+        return this.todos.filter(function(todo) {
           return !todo.isDone;
         });
-        return items.length;
       }
     }
   });
