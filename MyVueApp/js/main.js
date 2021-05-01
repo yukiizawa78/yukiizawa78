de0116e892e18f81fbb98b9e2191cf4ad45862dd
yukiizawa78 <artdirector1980@gmail.com>
2021-05-01 10:40:39 +0900

    make components

--- MyVueApp/js/main.js
+++ MyVueApp/js/main.js
@@ -1,55 +1,19 @@
 (function() {
   'use strict';
 
-  var vm = new Vue({
+  // Components
+
+  var likeComponent = Vue.extend({
+    template: '<button>Like</button>'
+    // template: '<button>Like</button><button>Like</button>'
+    // template: '<div><button>Like</button><button>Like</button></div>'
+  });
+
+  var app = new Vue({
     el: '#app',
-    data: {
-      newItem: '',
-      todos: []
-    },
-    watch: {
-      // todos: function() {
-      //   localStorage.setItem('todos', JSON.stringify(this.todos));
-      //   alert('Data saved!');
-      // }
-      todos: {
-        handler: function() {
-          localStorage.setItem('todos', JSON.stringify(this.todos));
-          // alert('Data saved!');
-        },
-        deep: true
-      }
-    },
-    mounted: function() {
-      this.todos = JSON.parse(localStorage.getItem('todos')) || [];
-    },
-    methods: {
-      addItem: function() {
-        var item = {
-          title: this.newItem,
-          isDone: false
-        };
-        this.todos.push(item);
-        this.newItem = '';
-      },
-      deleteItem: function(index) {
-        if (confirm('are you sure?')) {
-          this.todos.splice(index, 1);
-        }
-      },
-      purge: function() {
-        if (!confirm('delete finished?')) {
-          return;
-        }
-        this.todos = this.remaining;
-      }
-    },
-    computed: {
-      remaining: function() {
-        return this.todos.filter(function(todo) {
-          return !todo.isDone;
-        });
-      }
+    components: {
+      'like-component': likeComponent
     }
   });
+
 })();
