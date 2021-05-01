a0bbd16279e70d027c3bf0bd81e3e5786f99e512
yukiizawa78 <artdirector1980@gmail.com>
2021-05-01 11:50:26 +0900

    add try catch

--- JavaScriptObject/js/main.js
+++ JavaScriptObject/js/main.js
@@ -1,16 +1,14 @@
 'use strict';
 
 {
-  let i = 0;
+  // const name = 'taguchi';
+  const name = 5;
 
-  function showTime() {
-    console.log(new Date());
-    const timeoutId = setTimeout(showTime, 1000);
-    i++;
-    if (i > 2) {
-      clearTimeout(timeoutId);
-    }
+  try {
+    console.log(name.toUpperCase());
+  } catch (e) {
+    console.log(e);
   }
 
-  showTime();
+  console.log('Finish!');
 }
