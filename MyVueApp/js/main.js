9b600b341fea220b6855073bbfca6de7b9f0fd9c
yukiizawa78 <artdirector1980@gmail.com>
2021-05-01 10:54:38 +0900

    use props default

--- MyVueApp/js/main.js
+++ MyVueApp/js/main.js
@@ -4,12 +4,19 @@
   // Components
 
   var likeComponent = Vue.extend({
+    // props: ['message'],
+    props: {
+      message: {
+        type: String,
+        default: 'Like'
+      }
+    },
     data: function() {
       return {
         count: 0
       }
     },
-    template: '<button @click="countUp">Like {{ count }}</button>',
+    template: '<button @click="countUp">{{ message }} {{ count }}</button>',
     methods: {
       countUp: function() {
         this.count++;
