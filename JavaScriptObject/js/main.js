b89e185ad8799ca8bbca5c09b647e4ca6b3dea9d
yukiizawa78 <artdirector1980@gmail.com>
2021-05-01 12:44:01 +0900

    use Class

--- JavaScriptObject/js/main.js
+++ JavaScriptObject/js/main.js
@@ -1,25 +1,21 @@
 'use strict';
 
 {
-  const posts = [
-    {
-      text: 'JavaScriptの勉強中…',
-      likeCount: 0,
-      // show: function() {
-      //   console.log(`${this.text} - ${this.likeCount}いいね`);
-      // },
-      show() {
-        console.log(`${this.text} - ${this.likeCount}いいね`);
-      },
-    },
-    {
-      text: 'プログラミング楽しい！',
-      likeCount: 0,
-      show() {
-        console.log(`${this.text} - ${this.likeCount}いいね`);
-      },
-    },
-  ];
+      class Post {
+        constructor(text) {
+          this.text = text;
+          this.likeCount = 0;
+        }
+        
+          show() {
+            console.log(`${this.text} - ${this.likeCount}いいね`);
+          }
+        }
+      
+      const posts = [
+        new Post('JavaScriptの勉強中...'),
+        new Post('プログラミング楽しい！'),
+      ];
 
   // show(posts[0]);
   posts[0].show();
