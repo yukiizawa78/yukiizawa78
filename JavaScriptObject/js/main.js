909555d84d3fb46bd1317cf8cb12af532c9b2b71
yukiizawa78 <artdirector1980@gmail.com>
2021-05-01 12:10:31 +0900

    use methods in  object

--- JavaScriptObject/js/main.js
+++ JavaScriptObject/js/main.js
@@ -5,16 +5,23 @@
     {
       text: 'JavaScriptの勉強中…',
       likeCount: 0,
+      // show: function() {
+      //   console.log(`${this.text} - ${this.likeCount}いいね`);
+      // },
+      show() {
+        console.log(`${this.text} - ${this.likeCount}いいね`);
+      },
     },
     {
       text: 'プログラミング楽しい！',
       likeCount: 0,
+      show() {
+        console.log(`${this.text} - ${this.likeCount}いいね`);
+      },
     },
   ];
 
-  function show(post) {
-    console.log(`${post.text} - ${post.likeCount}いいね`);
-  }
-
-  show(posts[0]);
+  // show(posts[0]);
+  posts[0].show();
+  posts[1].show();
 }
