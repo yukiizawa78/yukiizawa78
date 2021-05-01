ad4a09df59b5f756d6d5b7d06a4eb3cf21072ce1
yukiizawa78 <artdirector1980@gmail.com>
2021-05-01 10:27:19 +0900

    add local storage

--- MyVueApp/js/main.js
+++ MyVueApp/js/main.js
@@ -5,16 +5,7 @@
     el: '#app',
     data: {
       newItem: '',
-      todos: [{
-        title: 'task 1',
-        isDone: false
-      }, {
-        title: 'task 2',
-        isDone: false
-      }, {
-        title: 'task 3',
-        isDone: true
-      }]
+      todos: []
     },
     watch: {
       // todos: function() {
@@ -28,6 +19,9 @@
         },
         deep: true
       }
+    },
+    mounted: function() {
+      this.todos = JSON.parse(localStorage.getItem('todos')) || [];
     },
     methods: {
       addItem: function() {
