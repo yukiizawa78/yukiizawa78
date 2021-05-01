5b4f83e692d289645977748c505eb9dfcf47da5c
yukiizawa78 <artdirector1980@gmail.com>
2021-05-01 10:16:44 +0900

    use deep true

--- MyVueApp/js/main.js
+++ MyVueApp/js/main.js
@@ -16,9 +16,22 @@
         isDone: true
       }]
     },
+    watch: {
+      // todos: function() {
+      //   localStorage.setItem('todos', JSON.stringify(this.todos));
+      //   alert('Data saved!');
+      // }
+      todos: {
+        handler: function() {
+          localStorage.setItem('todos', JSON.stringify(this.todos));
+          // alert('Data saved!');
+        },
+        deep: true
+      }
+    },
     methods: {
       addItem: function() {
-        let item = {
+        var item = {
           title: this.newItem,
           isDone: false
         };
@@ -34,18 +47,11 @@
         if (!confirm('delete finished?')) {
           return;
         }
-        // this.todos = this.todos.filter(function(todo) {
-        //   return !todo.isDone;
-        // }); 
         this.todos = this.remaining;
       }
     },
     computed: {
       remaining: function() {
-        // var items = this.todos.filter(function(todo) {
-        //   return !todo.isDone;
-        // });
-        // return items.length;
         return this.todos.filter(function(todo) {
           return !todo.isDone;
         });
