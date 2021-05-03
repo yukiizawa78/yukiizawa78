040c6603d4ab87287f2aa831762c05155925237a
yukiizawa78 <artdirector1980@gmail.com>
2021-05-04 00:13:06 +0900

    make class instance

--- JavaScriptObject/js/main.js
+++ JavaScriptObject/js/main.js
@@ -1,32 +1,46 @@
 'use strict';
 
 {
-      class Post {
-        constructor(text) {
-          this.text = text;
-          this.likeCount = 0;
-        }
-        
-          show() {
-            console.log(`${this.text} - ${this.likeCount} likes`);
-          }
+  class Post {
+    constructor(text) {
+      this.text = text;
+      this.likeCount = 0;
+    }
 
-          like() {
-            this.likeCount++;
-            this.show();
-          }
-          // 静的メソッド
-          // thisは使えない
-          static showInfo() {
-            console.log('Post class version 1.0');
-          }
-        }
-      
-      const posts = [
-        new Post('JavaScriptの勉強中...'),
-        new Post('プログラミング楽しい！'),
-      ];
+    show() {
+      console.log(`${this.text} - ${this.likeCount} likes`);
+    }
 
-  // posts[0].like();
-  Post.showInfo();
+    like() {
+      this.likeCount++;
+      this.show();
+    }
+  }
+
+  class SponsoredPost {
+    constructor(text, sponsor) {
+      this.text = text;
+      this.likeCount = 0;
+      this.sponsor = sponsor;
+    }
+
+    show() {
+      console.log(`${this.text} - ${this.likeCount} likes`);
+      console.log(`... sponsored by ${this.sponsor}`);
+    }
+
+    like() {
+      this.likeCount++;
+      this.show();
+    }
+  }
+
+  const posts = [
+    new Post('JavaScriptの勉強中…'),
+    new Post('プログラミング楽しい！'),
+    new SponsoredPost('3分動画でマスターしよう', 'dotinstall'),
+  ];
+
+  posts[2].show();
+  posts[2].like();
 }
