89acc59496c6d9e536119ab4a0864a61729a74df
yukiizawa78 <artdirector1980@gmail.com>
2021-05-09 12:20:27 +0900

    add isToday, isDisabled

--- MyCalendar/js/main.js
+++ MyCalendar/js/main.js
@@ -1,15 +1,21 @@
 'use strict';
 
+console.clear();
+
 {
   const year = 2020;
   const month = 4; // 5月
 
   function getCalendarBody() {
-    const dates = []; // date:日付, day:曜日
+    const dates = []; // date: 日付, day: 曜日
     const lastDate = new Date(year, month + 1, 0).getDate();
 
     for (let i = 1; i <= lastDate; i++) {
-      dates.push(i);
+      dates.push({
+        date: i,
+        isToday: false,
+        isDisabled: false,
+      });
     }
 
     console.log(dates);
