6cd76952a2fb4eafeaaf6106c516f7a5e037e81a
yukiizawa78 <artdirector1980@gmail.com>
2021-05-09 11:31:41 +0900

    add getCalenderBody

--- MyCalendar/js/main.js
+++ MyCalendar/js/main.js
@@ -3,4 +3,17 @@
 {
   const year = 2020;
   const month = 4; // 5月
+
+  function getCalendarBody() {
+    const dates = []; // date:日付, day:曜日
+    const lastDate = new Date(year, month + 1, 0).getDate();
+
+    for (let i = 1; i <= lastDate; i++) {
+      dates.push(i);
+    }
+
+    console.log(dates);
+  }
+
+  getCalendarBody();
 }
