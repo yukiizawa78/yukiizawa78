e0279dbdd346098c261f6fe82890db672883a552
yukiizawa78 <artdirector1980@gmail.com>
2021-05-09 21:18:59 +0900

    add getCalendarTail()

--- MyCalendar/js/main.js
+++ MyCalendar/js/main.js
@@ -40,6 +40,21 @@ console.clear();
     console.log(dates);
   }
 
-  getCalendarHead();
+  function getCalendarTail() {
+    const dates = [];
+    const lastDay = new Date(year, month + 1, 0).getDay();
+
+    for (let i = 1; i < 7 - lastDay; i++) {
+      dates.push({
+        date: i,
+        isToday: false,
+        isDisabled: true,
+      });
+    }
+    console.log(dates);
+  }
+
+  // getCalendarHead();
   // getCalendarBody();
+  getCalendarTail();
 }
